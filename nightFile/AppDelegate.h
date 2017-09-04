ec180a617c3ed0e2eb5d3e473506bc0e1f587b34
panshouheng <350621987@qq.com>
2017-09-04 17:55:31 +0800

    Initial Commit

---
+++ nightFile/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  nightFile
+//
+//  Created by apple on 2017/9/4.
+//  Copyright © 2017年 XiaMenYunYou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
